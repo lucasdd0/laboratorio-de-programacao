72115f335e93964177e81fe44f9f7a4fba7fc1c0
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-24 22:19:01 -0300

    commit

--- exercicios/src/exercicios/exercicio20.java
+++ exercicios/src/exercicios/exercicio20.java
@@ -1,5 +1,35 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class exercicio20 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		
+		int mm [][] = new int [3][3];
+		int i, j;
+		
+		System.out.println("Digite o total de linhas: ");
+		i = input.nextInt();
+		
+		System.out.println("Digite o total de colunas: ");
+		j = input.nextInt();
+		
+		for (i = 0; i < 3; i++) {
+			for (j = 0; j < 3; j++) {
+				System.out.println("Digite o [" + (i+1) + "]" + "[" + (j+1) + "] : ");
+				mm [i][j] = input.nextInt();
+			}
+		}
+		
+		for (i = 0; i < 3; i++) {
+			for (j = 0; j < 3; j++) {
+				System.out.print(mm [i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		input.close();
+	}
 
 }
