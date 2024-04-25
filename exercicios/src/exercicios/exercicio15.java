d3200d4e3c68016acdbaa529f8e7959ba94aa290
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-24 21:51:53 -0300

    comiit

---
+++ exercicios/src/exercicios/exercicio15.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio15 {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		String letra;
+
+		System.out.println("Digite uma letra: ");
+		letra = in.next();
+		letra = letra.toLowerCase();
+		in.close();
+
+		if (letra.equals("a") || letra.equals("e") || letra.equals("i") || letra.equals("o") || letra.equals("u")) {
+		System.out.println("É uma vogal");
+		} else
+			System.out.println("Não é uma vogal");
+	}
+}
---
+++ exercicios/src/exercicios/exercicio16.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio16 {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		String letra;
+        
+		System.out.println("Digite uma letra: ");
+		letra = in.next();
+		letra = letra.toLowerCase();
+		in.close();
+		
+		switch (letra) {
+		case "a":
+		case "e":
+		case "i":
+		case "o":
+		case "u":
+			System.out.println("É uma vogal");
+			break;
+			default:
+			System.out.println("Não é uma vogal");
+		}
+	}
+}
