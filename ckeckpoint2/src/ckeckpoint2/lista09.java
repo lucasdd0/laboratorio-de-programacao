f507ba86c7cd8795ba4ee351329a38014321ba81
lucasdd0 <lucasfo2005@gmail.com>
2024-05-15 15:07:37 -0300

    commit

---
+++ ckeckpoint2/src/ckeckpoint2/lista09.java
@@ -0,0 +1,10 @@
+package ckeckpoint2;
+
+import java.util.Scanner;
+
+public class lista09 {
+	public static void main(String[] args) {
+		Scanner in 
+	}
+
+}
--- exercicios-sala/src/exercicios/exercicio27.java
+++ exercicios-sala/src/exercicios/exercicio27.java
@@ -4,20 +4,31 @@ import java.util.Scanner;
 
 public class exercicio27 {
 	public static void main(String[] args) {
-		Scanner in = new Scanner (System.in);
+		Scanner in = new Scanner(System.in);
 		
 		int n, i;
+		boolean primo = true;
 		
 		System.out.println("Digite um número: ");
 		n = in.nextInt();
 		
-		for (i = 1; i <= n; i++) {
-			if(n / i == n && n / i == 1) {
-				System.out.println("É um número primo");
-			}else {
-				System.out.println("Não é um número primo");
+		if (n <= 1) {
+			primo = false;
+		}else {
+			for(i = 2; i <= n / 2; i++) {
+				if (n % i == 0) {
+					primo = false;
+					break;
+				}
 			}
 		}
+		
+		if (primo) {
+			 System.out.println(n + " é um número primo.");
+	     } else {
+	         System.err.println(n + " não é um número primo.");
+	     }
+		
 		
 		in.close();
 	}
