32a910a54186e949a0567e858d5bc8227dc1b925
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-08 22:46:18 -0300

    commit

---
+++ exercicios-sala/src/SegundoSemestre/exercicio24.java
@@ -0,0 +1,43 @@
+package SegundoSemestre;
+
+import java.util.Scanner;
+
+public class exercicio24 {
+	public static void main(String[] args) {
+		
+		String [] lista = new String [5];
+		
+		lerPalavra(lista);
+		localizarPar(lista);
+		
+	}
+	
+	public static void lerPalavra(String [] lista) {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < lista.length; i++) {
+			System.out.println("Digite a " + (i + 1) + "ª palavra: ");
+			lista[i] = sc.next();
+		}
+		
+	}
+	
+	public static void localizarPar(String [] lista) {
+		String invertida;
+		for (String palavra : lista) {
+			invertida = inverter(palavra);
+		}
+	}
+	
+	public static String inverter(String palavra) {
+		char[] letra = new char[palavra.length()];
+		for(int i = palavra.length() - 1; i >= 0; i--) {
+			letra[palavra.length() - 1 - i] = palavra.charAt(i);
+		}
+		String aux = new String(letra);
+		System.out.println(aux);
+		return aux;
+		
+	}
+	
+
+}
