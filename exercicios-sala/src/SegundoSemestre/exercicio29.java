9c8f1649e3e9795ad8bfd3bb5c96a08b92ba5503
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-22 21:48:40 -0300

    commit

---
+++ exercicios-sala/src/SegundoSemestre/exercicio29.java
@@ -0,0 +1,14 @@
+package SegundoSemestre;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class exercicio29 {
+	public static void main(String[] args) {
+		
+		FileReader file;
+		
+		BufferedReader
+	}
+
+}
