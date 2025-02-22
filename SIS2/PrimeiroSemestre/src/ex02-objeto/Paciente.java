9444839a013920d14b5f8edf76d56027d23da8ec
lucasdd0 <lucasfo2005@gmail.com>
2025-02-22 13:41:58 -0300

    commit

---
+++ SIS2/PrimeiroSemestre/src/ex02-objeto/Paciente.java
@@ -0,0 +1,18 @@
+public class Paciente {
+    String nome;
+    int idade, frequencia;
+
+    public int frequanciaMaxima() {
+        return 220 - idade;
+    }
+
+    // metodo para calcular e retornar a freq alvo
+    public double[] frequenciaAlvo() {
+        double[] alvo = new double[2];
+        double maxima = frequanciaMaxima();
+        alvo[0] = maxima * 0.5;
+        alvo[1] = maxima * 0.85;
+        return alvo;
+    }
+
+}
--- SIS2/PrimeiroSemestre/src/ex03-vetor/vetores.java
+++ SIS2/PrimeiroSemestre/src/ex03-vetor/vetores.java
@@ -1,5 +1,3 @@
-package ex03;
-
 import java.util.Scanner;
 
 public class vetores{
