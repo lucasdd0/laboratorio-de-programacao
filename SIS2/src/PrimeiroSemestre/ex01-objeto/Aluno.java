38a615b1d42b481beccf0f5a0e9bbdcbe248086b
lucasdd0 <lucasfo2005@gmail.com>
2025-03-16 23:12:43 -0300

    arrumando repositorios

--- SIS2/src/PrimeiroSemestre/ex01-objeto/Aluno.java
+++
@@ -1,9 +0,0 @@
-public class Aluno {
-    int rm;
-    String Nome;
-    double[] nota = new double[2];
-
-    public double CalcularMedia() {
-        return (nota[0] + nota[1]) / 2;
-    }
-}
--- SIS2/src/PrimeiroSemestre/ex04-objeto/pontuacaoJogos.java
+++
@@ -1,5 +0,0 @@
-public class pontuacaoJogos {
-    public static void main(String[] args) {
-
-    }
-}
